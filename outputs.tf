6bd107098069b38c7e2dc9cdbde0d878bd28c33b
JBerben <joshberben.work@gmail.com>
2025-03-11 17:13:12 +1100

    Added bucket id to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@
 output "hello_base_url" {
   value = "${aws_apigatewayv2_stage.dev.invoke_url}"
 }
+
+# Bucket where lambdas are deployed 
+output "test_s3_bucket" {
+  value = "${random_pet.lambda_bucket_name.id}"
+}
